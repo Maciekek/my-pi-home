77fd5d96c44c196d4ddcc49285349ec3e2d30e97
maciek pi <maciekek@gmail.com>
2019-07-30 16:20:56 +0100

    pi reader

--- pi/server/reader/piReader.js
+++ pi/server/reader/piReader.js
@@ -1,10 +1,26 @@
 const ds18b20 = require('ds18b20');
 
 const Reader = {
-    getValue: () => {
-        return ds18b20.temperatureSync('28-0000061ba8e4',{parser: 'hex'});
-    }
+	sensorNames: [],
+	getValues: () => {
+		console.log(sensorNames.map((sensorName) => { 
+	              	return {id: sensorName, value: ds18b20.temperatureSync(sensorName, {parser: 'hex'})};
+		}))
+	},
+	getSensorNames: () => {
+		ds18b20.sensors((err, ids) => {
+				sensorNames = ids;
+				Reader.getValues();
+			})
+	},
+
+
+
+//    getValue: () => {
+  //      return ds18b20.temperatureSync('28-0000061ba8e4',{parser: 'hex'});
+    //}
 };
-Reader.getValue();
+
+Reader.getSensorNames();
 
 module.exports = Reader;
