74ad3d6c3bc4b8c70aad1781e50aae237e5c5b88
Maciej  Kucharski <maciej.kucharski@scalac.io>
2019-07-30 22:41:01 +0200

    setimeout

--- pi/server/main.js
+++ pi/server/main.js
@@ -5,6 +5,12 @@ const LOCATION_ID = 1;
 
 class Main {
     constructor() {
+       this.readAndSendData();
+
+       setTimeout(this.readAndSendData, 360000)
+    }
+
+    readAndSendData = () => {
         const data = piReader.getValues().map(temp => {
             return {
                 value: temp.value,
@@ -17,12 +23,11 @@ class Main {
         data.map(singledata => {
             tempsService.addNewTemps(singledata);
         })
-
     }
 }
 setTimeout(() => {
     const main = new Main();
-}, 1000)
+}, 1000);
 
 
 
