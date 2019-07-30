f148ed94f9983b04328dfdae15b8d231db7fff59
Maciej  Kucharski <maciej.kucharski@scalac.io>
2019-07-30 22:33:23 +0200

    console log inseat debug

--- pi/server/services/tempServices.js
+++ pi/server/services/tempServices.js
@@ -7,10 +7,10 @@ const tempServices = {
         debug('addNewTemps - start');
         axios.post('http://77.55.217.143:8888/api/temps', body)
             .then(function (response) {
-                debug('addNewTemps success saved new temps values', body)
+                console.log('addNewTemps success saved new temps values', body)
             })
             .catch(function (error) {
-                debug('addNewTemps error', error.errno || error.response.data)
+                console.log('addNewTemps error', error.errno || error.response.data)
                 // console.log(error);
             });
     }
