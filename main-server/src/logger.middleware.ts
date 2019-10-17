03a8d6950481fa9ccca3f25c870ff787d2ed762d
Maciej  Kucharski <maciej.kucharski@scalac.io>
2019-10-17 19:14:06 +0200

    add logger

---
+++ main-server/src/logger.middleware.ts
@@ -0,0 +1,6 @@
+export function logger(req, res, next) {
+    console.log(`Request...`);
+    console.log(req.url);
+
+    next();
+}
--- main-server/src/main.ts
+++ main-server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {ValidationPipe} from '@nestjs/common';
+import {logger} from "./logger.middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api/swagger', app, document);
+  app.use(logger);
+
   await app.listen(8888);
 }
 bootstrap();
