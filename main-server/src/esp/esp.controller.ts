060138cdbeca9c1831ab97eca6468826d1989101
Maciej  Kucharski <maciej.kucharski@scalac.io>
2019-10-18 19:32:58 +0200

    update frontend

--- main-server/src/esp/esp.controller.ts
+++ main-server/src/esp/esp.controller.ts
@@ -12,7 +12,7 @@ export class EspController {
         console.log(params);
         const test = new AddTempDto();
         test.locationId = params.locationId;
-        test.sensorId = params.valuename;
+        test.sensorId = params.task;
         test.value = params.value;
         test.date = new Date().toISOString();
 
