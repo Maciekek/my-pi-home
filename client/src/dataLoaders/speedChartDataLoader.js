dea1e64dc2e0e21b339b83ebdfa350c39159856e
maciejkucharski <maciej.kucharski@senti1.com>
2019-12-23 10:36:31 +0100

    bug fix

--- client/src/dataLoaders/speedChartDataLoader.js
+++ client/src/dataLoaders/speedChartDataLoader.js
@@ -2,9 +2,11 @@ import {SensorsService} from "../services/sensors.service";
 
 const speedChartDataLoader = {
   get(params) {
+    if(!params.sensors) {
+      return;
+    }
 
     return SensorsService.getSensorValues(params.locationId, params).then(response => {
-
       return response.data;
     });
   },
