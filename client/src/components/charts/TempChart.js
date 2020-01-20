d51b14b446b912720d8ecd21de2af1491ae02cea
maciejkucharski <maciej.kucharski@senti1.com>
2020-01-20 08:13:14 +0100

    rename tooltip

--- client/src/components/charts/TempChart.js
+++ client/src/components/charts/TempChart.js
@@ -28,11 +28,10 @@ const options = {
 
   },
   tooltip: {
-    headerFormat: '<div class="chart-tooltip" style="font-size: 15px;">Godzina: {point.key}<br>',
+    headerFormat: '<div class="chart-tooltip" style="font-size: 15px;">{point.key}<br>',
     footerFormat: '</div>',
-    xDateFormat: '%d-%m %H:%M',
+    xDateFormat: 'Godzina: %H:%M;  Data: %d-%m',
     shared: false
-
   },
 
 };
