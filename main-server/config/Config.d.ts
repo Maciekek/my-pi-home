6b1e1a30936a4c2c57b7dbe1b6863ca3a75a4666
Maciej  Kucharski <maciej.kucharski@scalac.io>
2020-01-28 21:28:33 +0100

    alert

--- main-server/config/Config.d.ts
+++ main-server/config/Config.d.ts
@@ -1,8 +1,12 @@
 /* tslint:disable */
 /* eslint-disable */
-interface Config {
-  dbConfig: DbConfig;
-}
-interface DbConfig {
-  url: string;
+declare module "node-config-ts" {
+  interface IConfig {
+    dbConfig: DbConfig
+  }
+  interface DbConfig {
+    url: string
+  }
+  export const config: Config
+  export type Config = IConfig
 }
