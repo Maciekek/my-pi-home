9443bd5e07019620b01a648ca29d4431575ab4bd
maciejkucharski <maciej.kucharski@senti1.com>
2021-10-06 10:59:08 +0200

    update temp query

--- main-server/src/temps/temps.service.ts
+++ main-server/src/temps/temps.service.ts
@@ -1,31 +1,28 @@
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import {Temp} from './interfaces/temp.interface';
-import {AddTempDto} from "./dto/add-temp.dto";
+import { Temp } from './interfaces/temp.interface';
+import { AddTempDto } from './dto/add-temp.dto';
 
 @Injectable()
 export class TempsService {
-    constructor(@InjectModel('Temp') private readonly tempModel: Model<Temp>) {}
+  constructor(@InjectModel('Temp') private readonly tempModel: Model<Temp>) {}
 
-    async addTemp(addTempDto: AddTempDto): Promise<Temp> {
-        console.log(addTempDto);
-        const addedTemp = new this.tempModel(addTempDto);
-        return await addedTemp.save();
-    }
+  async addTemp(addTempDto: AddTempDto): Promise<Temp> {
+    console.log(addTempDto);
+    const addedTemp = new this.tempModel(addTempDto);
+    return await addedTemp.save();
+  }
 
-    async findAll(): Promise<Temp[]> {
-        return await this.tempModel.find().exec();
-    }
+  async findAll(): Promise<Temp[]> {
+    return await this.tempModel.find().exec();
+  }
 
-    async findLastN(locationId, n): Promise<Temp[]> {
-        console.log(n);
-        console.log(locationId);
-        // date: {$gte : '2019-09-29 15:38'}}
-        return await this.tempModel
-            .find({locationId})
-            .sort({_id: -1})
-            .limit(Number(n))
-            .exec();
-    }
+  async findLastN(locationId, n): Promise<Temp[]> {
+    return await this.tempModel
+      .sort({ date: -1 })
+      .find({ locationId })
+      .limit(Number(n))
+      .exec();
+  }
 }
