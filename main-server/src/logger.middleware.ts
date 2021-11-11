4200dd8500da576ab1d493f6ddbe5ba5173a19ac
maciejkucharski <maciej.kucharski@senti1.com>
2021-11-11 20:38:15 +0100

    test

--- main-server/src/logger.middleware.ts
+++ main-server/src/logger.middleware.ts
@@ -1,6 +1,7 @@
 export function logger(req, res, next) {
   console.log(`Request...`);
   console.log(req.url);
+  console.log(4, req);
 
   next();
 }
--- main-server/src/temps/temps.controller.ts
+++ main-server/src/temps/temps.controller.ts
@@ -1,25 +1,25 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
-import {Temp} from "./interfaces/temp.interface";
-import {TempsService} from "./temps.service";
-import {AddTempDto} from "./dto/add-temp.dto";
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Temp } from './interfaces/temp.interface';
+import { TempsService } from './temps.service';
+import { AddTempDto } from './dto/add-temp.dto';
 
 @Controller('/temps')
 export class TempsController {
-    constructor(private readonly tempsService: TempsService) {}
+  constructor(private readonly tempsService: TempsService) {}
 
-    @Post()
-    async addTemp(@Body() addTempDto: AddTempDto): Promise<Temp> {
-        return this.tempsService.addTemp(addTempDto);
-    }
+  @Post()
+  async addTemp(@Body() addTempDto: AddTempDto): Promise<Temp> {
+    console.log(12, 'POST [addTemp]');
+    return this.tempsService.addTemp(addTempDto);
+  }
 
-    @Get()
-    async findAll(): Promise<Temp[]> {
-        return this.tempsService.findAll();
-    }
+  @Get()
+  async findAll(): Promise<Temp[]> {
+    return this.tempsService.findAll();
+  }
 
-    @Get(':n/:locationId')
-    async findLastN(@Param('locationId') locationId: string, @Param('n') n: string): Promise<Temp[]> {
-        return this.tempsService.findLastN(locationId, n);
-    }
-
+  @Get(':n/:locationId')
+  async findLastN(@Param('locationId') locationId: string, @Param('n') n: string): Promise<Temp[]> {
+    return this.tempsService.findLastN(locationId, n);
+  }
 }
