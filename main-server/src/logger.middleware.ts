8b6cf537febdeaef3597b0cef8531cfe93a9bcee
maciej.kucharski <maciej.kucharski@senti1.com>
2023-01-22 22:20:53 +0100

    twillio app

--- main-server/src/logger.middleware.ts
+++ main-server/src/logger.middleware.ts
@@ -3,7 +3,7 @@ import { Request } from 'express';
 export function logger(req: Request, res, next) {
   console.log(`Request...`);
   console.log(req.url);
-  console.log(4, req.method);
+  console.log(4,1, req.method);
 
   next();
 }
