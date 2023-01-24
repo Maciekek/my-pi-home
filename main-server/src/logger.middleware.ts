50e1e732b2adf94633945b6952575602a73d8b65
maciej.kucharski <maciej.kucharski@senti1.com>
2023-01-24 20:38:02 +0100

    user logger instead of  .logs

--- main-server/src/logger.middleware.ts
+++ main-server/src/logger.middleware.ts
@@ -1,4 +1,9 @@
-export function logger(message, next) {
-  console.log(new Date().toLocaleString(), message)
+import { Request } from 'express';
+
+export function logger(req: Request, res, next) {
+  console.log(`Request...`);
+  console.log(req.url);
+  console.log(4,1, req.method);
+
   next();
 }
