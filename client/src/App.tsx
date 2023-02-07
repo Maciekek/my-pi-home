8246e3a3023a87f24553ec3768b18543bea63531
maciej.kucharski <maciej.kucharski@senti1.com>
2023-02-07 09:10:42 +0100

    docker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'react-toastify/dist/ReactToastify.min.css';
 import './styles/App.scss';
 
 import { ConfirmationModal } from 'components/ConfirmationModal';
