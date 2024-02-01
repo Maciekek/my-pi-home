fa7783988b7e45002055605c7b95499682e1f2de
maciej.kucharski <maciej.kucharski@senti1.com>
2024-02-01 19:06:08 +0100

    update fetcher delay time

--- fetcher/fetchData.js
+++ fetcher/fetchData.js
@@ -1,33 +1,36 @@
 import fetch from 'node-fetch';
 
 const fetchData = () => {
-  fetch('http://77.55.217.143/api/temps/5/5d83477c1d15b82553f8932f').then(results => {
-    return results.json()
-  }).then(data => {
-    data.map(temp => {
-      const requestPayload = {
-        value: temp.value,
-        date: temp.date,
-        locationId: temp.locationId,
-        sensorId: temp.sensorId
-      }
-      
-      
-      fetch("https://temperaturki.bieda.it/api/temps", {
-        method: "POST",
-        body: JSON.stringify(requestPayload),
-        headers: {
-          "Content-Type": "application/json",
-        }
-      }).then(() => {
-        console.log(20, 'Dane wysłane', JSON.stringify(requestPayload))
-      }).catch((e) => {
-        console.log(22, "Nie udało się wysłać danych", e)
-      })
+  fetch('http://77.55.217.143/api/temps/5/5d83477c1d15b82553f8932f')
+    .then((results) => {
+      return results.json();
     })
-  })
-}
+    .then((data) => {
+      data.map((temp) => {
+        const requestPayload = {
+          value: temp.value,
+          date: temp.date,
+          locationId: temp.locationId,
+          sensorId: temp.sensorId,
+        };
 
-console.log("Działam!");
+        fetch('https://temperaturki.bieda.it/api/temps', {
+          method: 'POST',
+          body: JSON.stringify(requestPayload),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(() => {
+            console.log(20, 'Dane wysłane', JSON.stringify(requestPayload));
+          })
+          .catch((e) => {
+            console.log(22, 'Nie udało się wysłać danych', e);
+          });
+      });
+    });
+};
+
+console.log('Działam!');
 fetchData();
-setInterval(fetchData, 600000);
+setInterval(fetchData, 300000);
